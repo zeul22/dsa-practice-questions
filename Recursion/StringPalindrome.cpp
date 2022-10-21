13c98da6c9b5b686dd28d4829e8efb58e0aa2f6e
RAHUL ANAND <anandrahul044@gmail.com>
2022-10-22 00:16:04 +0530

    Rec Question

--- Recursion/StringPalindrome.cpp
+++ Recursion/StringPalindrome.cpp
@@ -3,9 +3,39 @@
 using namespace std;
 int StringPalindrome(string word);
 
+void StringPalin(string arr, int size, int start, int end) // Two-Variable Solution
+{
+    if (arr[start] != arr[end])
+    {
+        cout << "Not Palindrome!" << endl;
+        return;
+    }
+    if (start >= end)
+    {
+        cout << "Palindrome!" << endl;
+        return;
+    }
+    StringPalin(arr, size, start + 1, end - 1);
+}
+void strPalindrome(string &x, int i)
+{
+    if (x[x.size() - i - 1] != x[i])
+    {
+        cout << "Not Palindrome!" << endl;
+        return;
+    }
+    if (i >= x.size() / 2)
+    {
+        cout << "Palindrome!" << endl;
+        return;
+    }
+    strPalindrome(x, i + 1);
+}
+
 int main()
 {
     StringPalindrome("ABCDCBA");
+    StringPalin("ABCDCBA", 7, 0, 6);
 }
 
 int StringPalindrome(string word)
