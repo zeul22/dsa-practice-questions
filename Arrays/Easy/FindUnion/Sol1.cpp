726c8f6e6e07e437bc031c407c6c1e605219b174
RAHUL ANAND <anandrahul044@gmail.com>
2022-10-29 17:09:15 +0530

    array Question

---
+++ Arrays/Easy/FindUnion/Sol1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Union(int *arr, int size, int *arr2, int size2);
+int main() {}
+
+void Union(int *arr, int size, int *arr2, int size2)
+{
+    int *arr3 = (int *)malloc((size + size2) * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size2; i++)
+        {
+            /* code */
+        }
+    }
+
+    int i = 0;
+    int j = 0;
+    while (i < size && j < size2)
+    {
+    }
+}
